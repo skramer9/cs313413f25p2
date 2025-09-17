8f14e14826be4349330f97cad94c72ca01041e57
skramer <skramer5@luc.edu>
2025-09-16 20:46:54 -0500

    Fixed errors in testRemove()

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -79,7 +79,7 @@ public class TestIterator {
     // TODO using assertEquals and List.of, express which values are left in the list
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
-    fail("Not yet implemented"); // remove this line when done
+    assertEquals(list, List.of(33,44,55,66));
   }
 
   @Test
