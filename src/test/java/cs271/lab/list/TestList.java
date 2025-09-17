5d17a2fb99fc0c70debfe73505b4ccb194ed4ead
skramer <skramer5@luc.edu>
2025-09-16 20:42:48 -0500

    Fixed errors in testContains()

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -46,10 +46,9 @@ public class TestList {
 
   @Test
   public void testContains() {
-    // TODO write assertions using
-    // list.contains(77)
-    // that hold before and after adding 77 to the list
-    fail("Not yet implemented"); // remove this line when done
+    assertFalse(list.contains(77));
+    list.add(77);
+    assertTrue(list.contains(77));
   }
 
   @Test
